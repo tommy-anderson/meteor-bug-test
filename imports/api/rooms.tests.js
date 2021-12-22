6f8e9d8f872814e077bfe7a21d8e2f4a29a9d431
tommy-anderson <alexursu@planable.io>
2021-12-22 19:43:11 +0200

    👌 console log some node process envs

--- imports/api/rooms.tests.js
+++ imports/api/rooms.tests.js
@@ -4,6 +4,7 @@ import { resetDatabase } from 'meteor/xolvio:cleaner';
 describe('test', () => {
     beforeEach(() => {
       console.log(JSON.stringify(Meteor))
+      console.log(process.env.NODE_ENV)
       resetDatabase();
     });
 
@@ -11,6 +12,5 @@ describe('test', () => {
         const a = 1
         const b = 2
         assert.equal(a+b,3)
-        console.table(Meteor)
       });
 })
